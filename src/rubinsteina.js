feba4bdcaf63b432750dbb22b2d55772ae088b74
Anton Suprunov <suprunov.anton@gmail.com>
2019-03-17 03:39:02 +0500

    slideshow with dummy source

---
+++ src/rubinsteina.js
@@ -0,0 +1,41 @@
+export default {
+  data: [
+    [
+      [
+        'rubinsteina_001.jpg',
+        'rubinsteina_002.jpg',
+        'rubinsteina_003.jpg',
+        'rubinsteina_004.jpg',
+      ],
+      [
+        'rubinsteina_005.jpg',
+        'rubinsteina_006.jpg',
+        'rubinsteina_007.jpg',
+        'rubinsteina_008.jpg',
+      ]
+    ],
+
+    [
+      [
+        'rubinsteina_009.jpg',
+        'rubinsteina_010.jpg',
+      ],
+      [
+        'rubinsteina_011.jpg',
+        'rubinsteina_012.jpg',
+      ]
+    ],
+    
+    [
+      [
+        'rubinsteina_013.jpg',
+        'rubinsteina_014.jpg',
+        'rubinsteina_015.jpg',
+        'rubinsteina_016.jpg',
+      ],
+      [
+        'rubinsteina_017.jpg',
+      ]
+    ]
+  ]
+};
